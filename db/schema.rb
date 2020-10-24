7ebc660530e9018ea73fc8055cb53992a15d711a
EC2 Default User <ec2-user@ip-172-31-4-218.us-east-2.compute.internal>
2020-10-24 21:24:40 +0000

    new column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201023124346) do
+ActiveRecord::Schema.define(version: 20201024210203) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20201023124346) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.integer  "weight"
   end
 
 end
